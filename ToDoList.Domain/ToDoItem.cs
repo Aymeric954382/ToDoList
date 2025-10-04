abc312bd1f8a992d8289ea037396a7798622f4e7
Aymeric954382 <estepanov172@gmail.com>
2025-10-04 15:05:16 +0300

    Добавьте файлы проекта.

---
+++ ToDoList.Domain/ToDoItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Domain
+{
+    public class ToDoItem
+    {
+        public Guid Id { get; private set; }
+        public Guid UserId { get; private set; }
+        public string Title { get; private set; }
+        public string Details { get; private set; }
+        public DateTime CreationDate { get; private set; }
+        public DateTime DateUpdate { get; private set; }
+        public ToDoStatus Status { get; private set; }
+    }
+}
---
+++ ToDoList.Domain/ValueObjects/ToDoStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Domain.ValueObjects
+{
+    public class ToDoStatus
+    {
+        public enum TodoStatus
+        {
+            Active,       
+            Completed,     
+            Expired,       
+            ExpiringSoon,  
+            Cancelled      
+        }
+    }
+}
