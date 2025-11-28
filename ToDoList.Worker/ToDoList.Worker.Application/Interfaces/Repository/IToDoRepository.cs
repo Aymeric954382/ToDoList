3cbce002d3417a0d4a9c9f22f47a8ac4e97cc2d1
Aymeric954382 <estepanov172@gmail.com>
2025-11-28 16:49:57 +0300

    folder fix

---
+++ ToDoList.Worker/ToDoList.Worker.Application/Interfaces/Repository/IToDoRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Worker.Domain;
+
+namespace ToDoList.Worker.Application.Repository
+{
+    public interface IToDoRepository
+    {
+        IQueryable<ToDoItem> AsQueryable();
+        Task<ToDoItem?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task<List<ToDoItem>> GetByUserIdAsync(Guid userId, CancellationToken cancellationToken);
+        Task AddAsync(ToDoItem todo, CancellationToken cancellationToken);
+        Task UpdateAsync(ToDoItem todo, CancellationToken cancellationToken);
+        Task DeleteAsync(ToDoItem todo, CancellationToken cancellationToken);
+    }
+}
