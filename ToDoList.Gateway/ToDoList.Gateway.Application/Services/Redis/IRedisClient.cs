81b247b636c491feb238c9b35053da985e9daaf2
Aymeric954382 <estepanov172@gmail.com>
2025-12-07 16:37:59 +0300

    Added Redis client with Deadlines + ZSET + HASH scheme

---
+++ ToDoList.Gateway/ToDoList.Gateway.Application/Services/Redis/IRedisClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Gateway.Contracts.ApiClients.Stubs;
+
+namespace ToDoList.Gateway.Application.Services.Redis
+{
+    public interface IRedisClient
+    {
+        Task AddDeadlineStubAsync(DeadLineStub stub);
+    }
+}
